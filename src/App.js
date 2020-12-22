7bc05bfcd07ad7e19fed84782f43b6e5c251a73d
FurkanKilicer <furkankcr58@gmail.com>
2020-12-23 02:46:57 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import ENTRY_LIST from "./data/data";
 import Footer from "./components/footer";
 
 export default function App() {
-  const CardComp = ENTRY_LIST.map((item, index) => (
+  const Cardexp = ENTRY_LIST.map((item, index) => (
     <Cards imgUrl={item.imgUrl} body={item.body} like={item.like} id={index} />
   ));
   return (
     <div>
       <Navbar />
       <div className="container">
-        <div className="row">{CardComp}</div>
+        <div className="row">{Cardexp}</div>
       </div>
       <Footer />
     </div>
